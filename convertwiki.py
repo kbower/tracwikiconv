9c721b2c2d7de56709620cd899a6c1e1c8beb3cb
Kent Bower <kb@retailarchitects.com>
2017-01-28 17:37:32 -0500

    wikinames collected

--- convertwiki.py
+++ convertwiki.py
@@ -1,14 +1,20 @@
 from sqlalchemy import *
 from sqlalchemy.orm import *
 
-engine = create_engine('sqlite:///', echo=True)
+engine = create_engine('sqlite:///../tracdb/trac.db', echo=True)
 metadata = MetaData(engine)
 metadata.reflect()
-
 
 def _convertWikiToMd(txt, currentticket):
     from trac.wiki.formatter import trac_to_github
     return trac_to_github(txt, _gitpath, currentticket)
+
+wiki_table = metadata.tables['wiki']
+
+stmt = select([distinct(wiki_table.c.name)])
+
+for name in engine.execute(stmt).fetchall():
+    print name.name
 
 import pdb; pdb.set_trace()
 
